e20e682fc7902fa24c149ad00ea7c6c770efa36d
Jos77we <josemaina207@gmail.com>
2024-01-03 20:06:58 +0300

    major changes

--- backend/routes/AccountRoute.js
+++ backend/routes/AccountRoute.js
@@ -82,4 +82,23 @@ router.get("/account-details", asyncHandler(async(req, res) =>{
 }))
 
 
+router.get("/detail-accounts", asyncHandler(async(req, res) =>{
+    const {idNo} = req.query
+
+    try {
+        const type = await account.find({idNo: {$in: idNo}})
+        // console.log('query list :', type)
+
+        if(type){
+            // res.status(200).json({accountName: type.accountName, accountNo: type.accountNo, amount: type.amount, status: type.status, cardNo: type.cardNo, lastDeposit: type.depositStamp, lastWithdrawal: type.withdrawStamp})
+            res.json({ data: type });
+        }else{
+            res.status(400).json({message:'Error occured'})
+        }
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}))
+
+
 module.exports = router
