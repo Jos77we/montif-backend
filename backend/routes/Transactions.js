70bb1b2a8059aa5c514496268e757b89862fb4fb
Jos77we <josemaina207@gmail.com>
2024-02-25 14:58:15 +0300

    Major change

--- backend/routes/Transactions.js
+++ backend/routes/Transactions.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const transact = require('../model/TransactionModel')
+const account = require("../model/Account")
 const user = require("../model/UsersModel")
 const asyncHandler = require('express-async-handler')
 
@@ -48,6 +49,38 @@ router.get("/all-transactions", asyncHandler(async(req, res) => {
         res.status(400).json(error)
     }
     
+}))
+
+router.post("/transcted", asyncHandler(async(req, res) => {
+    const {idNo, amount, accountName} = req.body
+
+    try {
+        const accountExist = await account.findOne({idNo, accountName});
+
+        if(!accountExist){
+            return res.status(400).json({message:"Invalid Account"})
+        }
+
+        if(amount < accountExist.amount) {
+            const stringVal1 = amount;
+            const floatNum = parseFloat(stringVal1)
+            const roundedVal1 = floatNum.toFixed(2)
+            const finalFloatValue1 = parseFloat(roundedVal1);
+
+            const stringVal2 = accountExist.amount;
+            const floatNum1 = parseFloat(stringVal2)
+            const roundedVal2 = floatNum1.toFixed(2)
+            const finalFloatValue2 = parseFloat(roundedVal2);
+
+            const newAmount = finalFloatValue2 - finalFloatValue1
+
+            return res.status(200).json({amount: newAmount, message: "New amount to be updated"})
+        }else{
+            return res.status(400).json({message:"Amount passed isn't valid"})
+        }
+    } catch (error) {
+        res.status(400).json(error)
+    }
 }))
 
 module.exports = router
