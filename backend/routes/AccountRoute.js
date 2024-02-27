7023ef7fe289433f3bdbea7fe2a56891d2ad3f00
Jos77we <josemaina207@gmail.com>
2024-02-27 12:18:06 +0300

    New endpoint

--- backend/routes/AccountRoute.js
+++ backend/routes/AccountRoute.js
@@ -1,104 +1,135 @@
-const express = require("express")
-const account = require("../model/Account")
-const asyncHandler = require('express-async-handler')
+const express = require("express");
+const account = require("../model/Account");
+const asyncHandler = require("express-async-handler");
 
-const router = express()
+const router = express();
 
-router.post("/new-account", asyncHandler(async(req, res) =>{
-    const {name, 
-        idNo, 
-        accountNo, 
-        accountName, 
-        cardNo, 
-        amount, 
-        depositStamp, 
-        withdrawStamp, 
-        status,
-        deductions, 
-        subscriptions} = req.body
+router.post(
+  "/new-account",
+  asyncHandler(async (req, res) => {
+    const {
+      name,
+      idNo,
+      accountNo,
+      accountName,
+      cardNo,
+      amount,
+      depositStamp,
+      withdrawStamp,
+      status,
+      deductions,
+      subscriptions,
+    } = req.body;
 
-        const newAccount = await account.create({name, 
-            idNo, 
-            accountNo, 
-            accountName, 
-            cardNo, 
-            amount, 
-            depositStamp, 
-            withdrawStamp, 
-            status,
-            deductions, 
-            subscriptions})
+    const newAccount = await account.create({
+      name,
+      idNo,
+      accountNo,
+      accountName,
+      cardNo,
+      amount,
+      depositStamp,
+      withdrawStamp,
+      status,
+      deductions,
+      subscriptions,
+    });
 
-            if (newAccount){
-                res.status(200).json({message:"Account created"})
-            }else {
-                res.status(400)
-                throw new Error('Error occured')
-            }
-
-}))
-
-router.get("/all-accounts", asyncHandler(async(req, res) => {
-    try {
-        const allAccounts = await account.find({})
-        res.status(200).json(allAccounts)
-        
-    } catch (error) {
-        res.status(400).json(error)
+    if (newAccount) {
+      res.status(200).json({ message: "Account created" });
+    } else {
+      res.status(400);
+      throw new Error("Error occured");
     }
-   
-}))
+  })
+);
 
-router.get("/account-type", asyncHandler(async(req, res) =>{
-    const {idNo} = req.query
-
+router.get(
+  "/all-accounts",
+  asyncHandler(async (req, res) => {
     try {
-        const type = await account.findOne({idNo: {$in: idNo}})
-
-        if(type){
-            res.status(200).json({accountName: type.accountName, accountNo: type.accountNo, amount: type.amount, status: type.status})
-        }else{
-            res.status(400).json({message:'Error occured'})
-        }
+      const allAccounts = await account.find({});
+      res.status(200).json(allAccounts);
     } catch (error) {
-        res.status(400).json(error)
+      res.status(400).json(error);
     }
-}))
+  })
+);
 
-router.get("/account-details", asyncHandler(async(req, res) =>{
-    const {idNo} = req.query
+router.get(
+  "/account-type",
+  asyncHandler(async (req, res) => {
+    const { idNo } = req.query;
 
     try {
-        const type = await account.findOne({idNo: {$in: idNo}})
+      const type = await account.findOne({ idNo: { $in: idNo } });
 
-        if(type){
-            res.status(200).json({accountName: type.accountName, accountNo: type.accountNo, amount: type.amount, status: type.status, cardNo: type.cardNo, lastDeposit: type.depositStamp, lastWithdrawal: type.withdrawStamp})
-        }else{
-            res.status(400).json({message:'Error occured'})
-        }
+      if (type) {
+        res
+          .status(200)
+          .json({
+            accountName: type.accountName,
+            accountNo: type.accountNo,
+            amount: type.amount,
+            status: type.status,
+          });
+      } else {
+        res.status(400).json({ message: "Error occured" });
+      }
     } catch (error) {
-        res.status(400).json(error)
+      res.status(400).json(error);
     }
-}))
+  })
+);
 
-
-router.get("/detail-accounts", asyncHandler(async(req, res) =>{
-    const {idNo} = req.query
+router.get(
+  "/account-details",
+  asyncHandler(async (req, res) => {
+    const { idNo } = req.query;
 
     try {
-        const type = await account.find({idNo: {$in: idNo}})
-        // console.log('query list :', type)
+      const type = await account.findOne({ idNo: { $in: idNo } });
 
-        if(type){
-            // res.status(200).json({accountName: type.accountName, accountNo: type.accountNo, amount: type.amount, status: type.status, cardNo: type.cardNo, lastDeposit: type.depositStamp, lastWithdrawal: type.withdrawStamp})
-            res.json({ data: type });
-        }else{
-            res.status(400).json({message:'Error occured'})
-        }
+      if (type) {
+        res
+          .status(200)
+          .json({
+            accountName: type.accountName,
+            accountNo: type.accountNo,
+            amount: type.amount,
+            status: type.status,
+            cardNo: type.cardNo,
+            lastDeposit: type.depositStamp,
+            lastWithdrawal: type.withdrawStamp,
+          });
+      } else {
+        res.status(400).json({ message: "Error occured" });
+      }
     } catch (error) {
-        res.status(400).json(error)
+      res.status(400).json(error);
     }
-}))
+  })
+);
 
+router.get(
+  "/detail-accounts",
+  asyncHandler(async (req, res) => {
+    const { idNo } = req.query;
 
-module.exports = router
+    try {
+      const type = await account.find({ idNo: { $in: idNo } });
+      // console.log('query list :', type)
+
+      if (type) {
+        // res.status(200).json({accountName: type.accountName, accountNo: type.accountNo, amount: type.amount, status: type.status, cardNo: type.cardNo, lastDeposit: type.depositStamp, lastWithdrawal: type.withdrawStamp})
+        res.json({ data: type });
+      } else {
+        res.status(400).json({ message: "Error occured" });
+      }
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  })
+);
+
+module.exports = router;
--- backend/routes/Transactions.js
+++ backend/routes/Transactions.js
@@ -1,86 +1,104 @@
-const express = require("express")
-const transact = require('../model/TransactionModel')
-const account = require("../model/Account")
-const user = require("../model/UsersModel")
-const asyncHandler = require('express-async-handler')
+const express = require("express");
+const transact = require("../model/TransactionModel");
+const account = require("../model/Account");
+const user = require("../model/UsersModel");
+const asyncHandler = require("express-async-handler");
 
-const router = express()
+const router = express();
 
-router.post("/new-transaction", asyncHandler(async(req, res) => {
+
+router.get(
+  "/all-transactions",
+  asyncHandler(async (req, res) => {
+    try {
+      const allTransactions = await transact.find().sort({ createdAt: -1 });
+      res.status(200).json(allTransactions);
+    } catch (error) {
+      res.status(400).json(error);
+    }
+  })
+);
+
+router.post(
+  "/transacted",
+  asyncHandler(async (req, res) => {
     const {
-    name,
-    idNo,
-    transactionId,
-    accountNo,
-    typeOfTransaction,
-    amount,
-    recieverId,
-    receiverAccountNo
-    } = req.body
+      name,
+      idNo,
+      transactionId,
+      accountNo,
+      typeOfTransaction,
+      amount,
+      recieverId,
+      receiverAccountNo,
+      accountName,
+    } = req.body;
 
-    const verify = await user.findOne({idNo, name})
-    if(!verify){
-        res.status(400).json({statusCode: 9001, message:"Access restricted"})
+    const verify = await user.findOne({ idNo, name });
+    if (!verify) {
+      res.status(400).json({ statusCode: 9001, message: "User unknown" });
     }
-
-    const newTransaction = transact.create({name,
-        idNo,
-        transactionId,
-        accountNo,
-        typeOfTransaction,
-        amount,
-        recieverId,
-        receiverAccountNo})
-
-        if(newTransaction){
-            res.status(200).json({message: "Succesful transaction"})
-        }else{
-            res.status(400)
-            throw new Error("Transaction Failed")
-        }
-}))
-
-router.get("/all-transactions", asyncHandler(async(req, res) => {
-    try {
-        const allTransactions = await transact.find().sort({ createdAt: -1 });
-        res.status(200).json(allTransactions)
-        
-    } catch (error) {
-        res.status(400).json(error)
-    }
-    
-}))
-
-router.post("/transcted", asyncHandler(async(req, res) => {
-    const {idNo, amount, accountName} = req.body
 
     try {
-        const accountExist = await account.findOne({idNo, accountName});
+      const accountExist = await account.findOne({ idNo, accountName });
 
-        if(!accountExist){
-            return res.status(400).json({message:"Invalid Account"})
+      if (!accountExist) {
+        return res.status(400).json({ message: "Invalid Account" });
+      }
+
+      const stringVal1 = amount;
+      const floatNum = parseFloat(stringVal1);
+      const roundedVal1 = floatNum.toFixed(2);
+      const finalFloatValue1 = parseFloat(roundedVal1);
+
+      const stringVal2 = accountExist.amount;
+      const floatNum1 = parseFloat(stringVal2);
+      const roundedVal2 = floatNum1.toFixed(2);
+      const finalFloatValue2 = parseFloat(roundedVal2);
+
+      if (finalFloatValue1 > finalFloatValue2) {
+        return res
+          .status(400)
+          .json({ amount: amount, message: "The amount passed is invalid" });
+      } else {
+        const newAmount = finalFloatValue2 - finalFloatValue1;
+        const stringValue = newAmount.toString();
+
+        const newTransaction = transact.create({
+          name,
+          idNo,
+          transactionId,
+          accountNo,
+          typeOfTransaction,
+          amount,
+          recieverId,
+          receiverAccountNo,
+        });
+
+        if (newTransaction) {
+          const updatedValue = await account.findOneAndUpdate(
+            { _id: accountExist._id }, // Use _id to identify the document
+            { amount: stringValue }, // Update the 'amount' field
+            { new: true }
+          );
+          if (!updatedValue) {
+            res
+              .status(400)
+              .json({ statusCode: 9000, message: "Could not update value" });
+          }
+        } else {
+          res.status(400);
+          throw new Error("Transaction Failed");
         }
 
-        if(amount < accountExist.amount) {
-            const stringVal1 = amount;
-            const floatNum = parseFloat(stringVal1)
-            const roundedVal1 = floatNum.toFixed(2)
-            const finalFloatValue1 = parseFloat(roundedVal1);
-
-            const stringVal2 = accountExist.amount;
-            const floatNum1 = parseFloat(stringVal2)
-            const roundedVal2 = floatNum1.toFixed(2)
-            const finalFloatValue2 = parseFloat(roundedVal2);
-
-            const newAmount = finalFloatValue2 - finalFloatValue1
-
-            return res.status(200).json({amount: newAmount, message: "New amount to be updated"})
-        }else{
-            return res.status(400).json({message:"Amount passed isn't valid"})
-        }
+        return res
+          .status(200)
+          .json({ amount: stringValue, message: "New amount to be updated" });
+      }
     } catch (error) {
-        res.status(400).json(error)
+      res.status(400).json(error);
     }
-}))
+  })
+);
 
-module.exports = router
+module.exports = router;
